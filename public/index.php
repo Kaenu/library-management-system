a8792bf7a17ca90d77aade83e14ae4d44edd8608
Kanusan Karunarajah <kanusan1@hotmail.ch>
2023-09-20 09:21:59 +0200

    slim-framework

--- public/index.php
+++ public/index.php
@@ -2,3 +2,6 @@
 <?php
 phpinfo();
 ?>
+
+/** ?dasdas
+ */
---
+++ src/routes.php
@@ -0,0 +1,10 @@
+// routes.php
+
+// User routes
+require_once 'userRoutes.php';
+
+// Book routes
+require_once 'bookRoutes.php';
+
+// Admin routes
+require_once 'adminRoutes.php';
